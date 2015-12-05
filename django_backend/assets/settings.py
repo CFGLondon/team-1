86d6bfeb931b92b19d77e6e78c04b06e8f17e261
Teun Kokke <teun@icanplay.co.uk>
2015-12-05 03:32:58 +0100

    added random settings to try getting crossstuffs to work

--- django_backend/assets/settings.py
+++ django_backend/assets/settings.py
@@ -92,6 +92,14 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+ACCESS_CONTROL_ALLOW_ORIGIN = 'Access-Control-Allow-Origin'
+ACCESS_CONTROL_EXPOSE_HEADERS = 'Access-Control-Expose-Headers'
+ACCESS_CONTROL_ALLOW_CREDENTIALS = 'Access-Control-Allow-Credentials'
+ACCESS_CONTROL_ALLOW_HEADERS = 'Access-Control-Allow-Headers'
+ACCESS_CONTROL_ALLOW_METHODS = 'Access-Control-Allow-Methods'
+ACCESS_CONTROL_MAX_AGE = 'Access-Control-Max-Age'
+
+
 ROOT_URLCONF = 'assets.urls'
 
 WSGI_APPLICATION = 'assets.wsgi.application'
